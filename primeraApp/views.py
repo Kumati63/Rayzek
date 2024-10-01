c8c5b86943162415841e9add5e59dbc34e1cd0d5
JoffH <jeffery.hurtado@inacapmail.cl>
2024-10-01 02:42:55 -0300

    Crud notificaciones avanzado
    Crud de notificaciones que crea notificaciones, debemos decidir si lo haremos con este metodo temporal o haremos que se hagan cambios en el modelo, deje todo comentado para que se entienda.

--- primeraApp/views.py
+++ primeraApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import datetime
+from primeraApp.models import alertas
+from datetime import datetime
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def landingPage(request):
@@ -15,5 +17,33 @@ def Signup(request):
 def main(request):
     return render(request,'PrimeraApp\main.html')
 
+
+"""
+Este es un método temporal para guardar cosas en la página. El modelo no se actualiza con los datos y al reiniciar la página se eliminan.
+Para cambiar eso, debemos trabajar o con una base de datos o un JSON.
+"""
 def CrudNotificaciones(request):
-    return render(request,'PrimeraApp\CrudNotificaciones.html')
+    # Comprueba si llegan datos del formulario
+    if request.method == 'POST':
+        # Obteniene los datos del formulario
+        alerta = request.POST.get('alerta')
+        timestamp = request.POST.get('timestamp')
+        usuario = request.POST.get('usuario')
+
+        # Crear una nueva alerta 
+        new_id = max(alertas.keys()) + 1  # Asigna el siguiente ID disponible
+        alertas[new_id] = {
+            "id": new_id,
+            "alerta": alerta,
+            "timestamp": datetime.strptime(timestamp, '%Y-%m-%dT%H:%M'),
+            "usuario": usuario
+        }
+
+        # Redirigir a la página de notificaciones para evitar el reenvío del formulario al refrescar
+        return redirect('CrudNotificaciones')
+    #Envia datos del modelo 
+    data = {
+        'alertas': alertas
+    }
+    return render(request,'PrimeraApp\CrudNotificaciones.html',data)
+
